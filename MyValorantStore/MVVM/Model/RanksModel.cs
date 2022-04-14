294e68316b654a56e77ed1356bb691d52b3101ff
arbaa <arbaa@BAZPC>
2022-04-13 19:15:12 -0500

    Add project files.

---
+++ MyValorantStore/MVVM/Model/RanksModel.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyValorantStore.MVVM.Model
+{
+    public partial class RanksModel
+    {
+        [JsonProperty("actId")]
+        public Guid ActId { get; set; }
+
+        [JsonProperty("players")]
+        public Player[] Players { get; set; }
+
+        [JsonProperty("totalPlayers")]
+        public long TotalPlayers { get; set; }
+
+        [JsonProperty("immortalStartingPage")]
+        public long ImmortalStartingPage { get; set; }
+
+        [JsonProperty("immortalStartingIndex")]
+        public long ImmortalStartingIndex { get; set; }
+
+        [JsonProperty("topTierRRThreshold")]
+        public long TopTierRrThreshold { get; set; }
+
+        [JsonProperty("tierDetails")]
+        public Dictionary<string, TierDetail> TierDetails { get; set; }
+
+        [JsonProperty("startIndex")]
+        public long StartIndex { get; set; }
+
+        [JsonProperty("query")]
+        public string Query { get; set; }
+
+        [JsonProperty("shard")]
+        public string Shard { get; set; }
+    }
+
+    public partial class Player
+    {
+        [JsonProperty("puuid")]
+        public string Puuid { get; set; }
+
+        [JsonProperty("gameName")]
+        public string GameName { get; set; }
+
+        [JsonProperty("tagLine")]
+        public string TagLine { get; set; }
+
+        [JsonProperty("leaderboardRank")]
+        public long LeaderboardRank { get; set; }
+
+        [JsonProperty("rankedRating")]
+        public long RankedRating { get; set; }
+
+        [JsonProperty("numberOfWins")]
+        public long NumberOfWins { get; set; }
+
+        [JsonProperty("competitiveTier")]
+        public long CompetitiveTier { get; set; }
+    }
+
+    public partial class TierDetail
+    {
+        [JsonProperty("rankedRatingThreshold")]
+        public long RankedRatingThreshold { get; set; }
+
+        [JsonProperty("startingPage")]
+        public long StartingPage { get; set; }
+
+        [JsonProperty("startingIndex")]
+        public long StartingIndex { get; set; }
+    }
+}
---
+++ MyValorantStore/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using MyValorantStore.MVVM.Model;
+using System;
+using RestSharp;
+using ValNet;
+using ValNet.Objects.Authentication;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp.Authenticators.OAuth2;
+using Newtonsoft.Json;
+
+namespace MyValorantStore.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public ObservableCollection<Player> Players { get; set; }
+
+        public MainViewModel()
+        {
+            Players = new ObservableCollection<Player>();
+            PopulateCollection();
+        }
+
+        void PopulateCollection()
+        {
+            var client = new RestClient();
+            
+            /*client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator("RGAPI-11caeb2c-9a1b-4614-92ec-5911e0cd522c", "Bearer");*/
+            
+
+            var request = new RestRequest("https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/d929bc38-4ab6-7da4-94f0-ee84f8ac141e?size=50&startIndex=0&api_key=RGAPI-11caeb2c-9a1b-4614-92ec-5911e0cd522c", Method.Get);
+            request.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0");
+            request.AddHeader("Accept-Language", "en-US,en;q=0.5");
+            request.AddHeader("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8");
+            request.AddHeader("Origin", "https://developer.riotgames.com");
+
+            var response = client.GetAsync(request).GetAwaiter().GetResult();
+            var data = JsonConvert.DeserializeObject<RanksModel>(response.Content);
+
+            for (int i = 0; i<data.Players.Length; i++)
+            {
+                var player = data.Players[i];
+                player.RankedRating = data.Players[i].RankedRating;
+                Players.Add(player);
+            }
+        }
+    }
+}
